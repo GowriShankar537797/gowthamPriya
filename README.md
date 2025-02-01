79a5c7c339ab9fc8bd0af260ea2f4327a420c9f0
GowriShankarM <gowrishankar.m@aximsoft.com>
2025-02-01 23:02:20 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Wedding-Invitation
+
+echo "# gowthamPriya" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/GowriShankar537797/gowthamPriya.git
+git push -u origin main
---
+++ constant.js
@@ -0,0 +1,2 @@
+const kovil_Location =
+  "https://www.google.com/maps/place/Sri+Thanthondreeswarar+Temple+-+Belur/@11.7089916,78.4037352,473m/data=!3m1!1e3!4m6!3m5!1s0x3bab8e8d746040ef:0xa467ec68cf702fbb!8m2!3d11.7076687!4d78.4044047!16s%2Fm%2F0g53tn1?entry=ttu&g_ep=EgoyMDI1MDEyOS4xIKXMDSoASAFQAw%3D%3D";
